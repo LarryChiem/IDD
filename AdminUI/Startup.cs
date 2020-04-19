426713676ba72221c7b3c62030eb783fd37f95e3
David Post <post@pdx.edu>
2020-04-18 17:49:46 -0700

    Added Ability to Connect to azure or localdb (#58)
    * Connected to Azure
    
    Added Connections for Azure DB, AdminUI now connects to Azure instead of localdb
    
    Signed-off-by John Brusaw <jbrusaw@pdx.edu>
    
    * Set up the AdminUI to use Azure App Settings
    
    Now the non-development version should be using Azure App Settings
    Connection Strings to get credentials.
    
    Signed-off-by: David Post <post@pdx.edu>
    
    * Removed Connection String in appsettings.json
    
    The appsettings.Development.json has the localdb connection string
    and the deployment server should setup the connection string for
    running on the actual db.
    
    Signed-off-by: David Post <post@pdx.edu>
    Co-authored-by: John Brusaw <jwbrusaw@gmail.com>

--- AdminUI/Startup.cs
+++ AdminUI/Startup.cs
@@ -29,12 +29,13 @@ namespace AdminUI
         {
             services.AddControllersWithViews();
             services.AddProgressiveWebApp();
+
             services.AddDbContext<TimesheetContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("localdb")));
+                options.UseSqlServer(Configuration.GetConnectionString("AzureDB")));
             services.AddDbContext<AdminAccountContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("localdb")));
+                options.UseSqlServer(Configuration.GetConnectionString("AzureDB")));
             services.AddDbContext<LockTableContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("localdb")));
+                options.UseSqlServer(Configuration.GetConnectionString("AzureDB")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<AdminAccountContext>();
             services.AddRazorPages();

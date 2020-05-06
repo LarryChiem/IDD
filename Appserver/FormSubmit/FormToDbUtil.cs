1ea256ab8c42ce604db38e695ec3c2ba1b61545e
David Post <post@pdx.edu>
2020-05-06 12:03:29 -0700

    Changed URI to one in Azure Storage
    This is a quick fix to have this part working to show the AdminUI,
    but it needs to pull from Stagings table.
    
    Signed-off-by: David Post <post@pdx.edu>

--- Appserver/FormSubmit/FormToDbUtil.cs
+++ Appserver/FormSubmit/FormToDbUtil.cs
@@ -89,89 +89,6 @@ namespace IDD
             return totalInserts;
         }
 
-        // Submit the timesheet portion first to get the insert ID back.
-        // TODO this could be improved by making 'timesheet' a variable in
-        // the query portion, change the name of the method to something more
-        // general, and it can acommodate multiple form types....?
-        // FIXME add try/catch
-        public string TimesheetToDB(Timesheet ts)
-        {
-            decimal insertId = 0;
-
-            string query = "INSERT INTO [dbo].[Submissions] ([Submitted],[FormType],[ProviderName]";
-            query += ",[ProviderId],[ClientName],[ClientPrime],[ServiceGoal],[ProgressNotes],[Status]";
-            query += ",[UserActivity],[RejectionReason],[LockInfoId],[UriString],[Discriminator],[TotalMiles],[TotalHours]) ";
-            query += "VALUES(@submitted, @formtype, @providername, @providerid, @clientname, @clientprime, @servicegoal,";
-            query += "@progressnotes, @status, @useractivity, @rejectionreason, @lockinfoid, @uristring, @discriminator,";
-            query += "@totalmiles, @totalhours) SELECT SCOPE_IDENTITY();";
-
-            string conn_str = Environment.GetEnvironmentVariable("test-db-conn-str");
-            using (SqlConnection conn = new SqlConnection(conn_str))
-            {
-                SqlCommand command = new SqlCommand(query, conn);
-                command.Parameters.Add("@submitted", System.Data.SqlDbType.DateTime2);
-                command.Parameters["@submitted"].Value = ts.Submitted;
-
-                command.Parameters.Add("@formtype", System.Data.SqlDbType.NVarChar, 50);
-                command.Parameters["@formtype"].Value = ts.FormType;
-
-                command.Parameters.Add("@providername", System.Data.SqlDbType.NVarChar, 50);
-                command.Parameters["@providername"].Value = ts.ProviderName;
-
-                command.Parameters.Add("@providerid", System.Data.SqlDbType.NVarChar, 50);
-                command.Parameters["@providerid"].Value = ts.ProviderId;
-
-                command.Parameters.Add("@clientname", System.Data.SqlDbType.NVarChar, 50);
-                command.Parameters["@clientname"].Value = ts.ClientName;
-
-                command.Parameters.Add("@clientprime", System.Data.SqlDbType.NVarChar, 50);
-                command.Parameters["@clientprime"].Value = ts.ClientPrime;
-
-                command.Parameters.Add("@servicegoal", System.Data.SqlDbType.NVarChar, 100);
-                command.Parameters["@servicegoal"].Value = ts.ServiceGoal;
-
-                command.Parameters.Add("@progressnotes", System.Data.SqlDbType.NVarChar, 500);
-                command.Parameters["@progressnotes"].Value = ts.ProgressNotes;
-
-                command.Parameters.Add("@status", System.Data.SqlDbType.NVarChar, 50);
-                command.Parameters["@status"].Value = ts.Status;
-
-                command.Parameters.Add("@useractivity", System.Data.SqlDbType.NVarChar, 100);
-                command.Parameters["@useractivity"].Value = ts.UserActivity;
-
-                command.Parameters.Add("@rejectionreason", System.Data.SqlDbType.NVarChar, 250);
-                command.Parameters["@rejectionreason"].Value = ts.RejectionReason;
-
-                command.Parameters.Add("@lockinfoid", System.Data.SqlDbType.Int);
-                command.Parameters["@lockinfoid"].Value = DBNull.Value;
-
-                command.Parameters.Add("@uristring", System.Data.SqlDbType.NVarChar, 500);
-                command.Parameters["@uristring"].Value = ts.UriString;
-
-                command.Parameters.Add("@discriminator", System.Data.SqlDbType.NVarChar, 50);
-                command.Parameters["@discriminator"].Value = "Timesheet";
-
-                command.Parameters.Add("@totalmiles", System.Data.SqlDbType.Float);
-                command.Parameters["@totalmiles"].Value = 0;
-
-                command.Parameters.Add("@totalhours", System.Data.SqlDbType.Float);
-                command.Parameters["@totalhours"].Value = ts.TotalHours;
-
-                conn.Open();
-
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        insertId = reader.GetDecimal(0);
-                    }
-                    reader.Close();
-                }
-            }
-
-            return insertId.ToString();
-        }
-
         // Give a timesheetform obj, get back a partially populated timesheet obj.
         // TODO fix UriString, confirm vals for // marks
         public Timesheet PopulateTimesheet(TimesheetForm tsf, Timesheet tsheet=null)
@@ -189,7 +106,7 @@ namespace IDD
             tsheet.Submitted = DateTime.Now; //
             tsheet.LockInfo = null;
             tsheet.UserActivity = "none"; //
-            tsheet.UriString = "somehandle, someotherhandle"; //
+            tsheet.UriString = "https://iddstorageaccountdev.blob.core.windows.net/submissionfiles/OR507_19-11-1.png, https://iddstorageaccountdev.blob.core.windows.net/submissionfiles/OR507_19-11-1.png"; //
 
             return tsheet;
         }

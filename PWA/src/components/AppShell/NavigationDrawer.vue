1d12f7a4c54af8a05fd430c9ab3ee2d3fc3c94eb
David Post <post@pdx.edu>
2020-05-19 22:28:47 -0700

    Added Link to report bugs in PWA and Mileage Form
    Adds in a way to report bugs and also adds in mileage form
    support

--- PWA/src/components/AppShell/NavigationDrawer.vue
+++ PWA/src/components/AppShell/NavigationDrawer.vue
@@ -1,10 +1,5 @@
 <template>
-  <v-navigation-drawer
-    app
-    color="grey lighten-4"
-    v-model="drawerOpen"
-    @input="onInput"
-  >
+  <v-navigation-drawer app color="grey lighten-4" v-model="drawerOpen" @input="onInput">
     <v-list dense>
       <!-- Link to homepage -->
       <v-list-item link to="/">
@@ -35,39 +30,55 @@
           <v-list-item-title>About</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
+
+      <!-- Link to the about page -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-list-item :href="bugReport" target="_blank" v-on="on">
+            <v-list-item-action>
+              <v-icon>mdi-bug</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Report a bug</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+        <span>Report a bug</span>
+      </v-tooltip>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
-  export default {
-    name: "NavigationDrawer",
-    props: {
-      // Display toggle value passed in from the parent component
-      open: {
-        type: Boolean,
-        default: false,
-      },
-    },
-    data: function () {
-      return {
-        // Display toggle; if true, display navigation drawer, else hide
-        drawerOpen: this.open,
-      };
-    },
-    // Parent -> Child communication
-    watch: {
-      // Change should come from pressing a button on the AppBar
-      open(newVal) {
-        this.drawerOpen = newVal;
-      },
-    },
-    // Child -> Parent communication
-    methods: {
-      // Change 'isOpen' upon closing the navigation drawer
-      onInput(isOpen) {
-        this.$emit("drawer-change", isOpen);
-      },
-    },
-  };
+export default {
+  name: "NavigationDrawer",
+  props: {
+    // Display toggle value passed in from the parent component
+    open: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: function() {
+    return {
+      // Display toggle; if true, display navigation drawer, else hide
+      drawerOpen: this.open,
+      bugReport: process.env.VUE_APP_BUG_REPORT
+    };
+  },
+  // Parent -> Child communication
+  watch: {
+    // Change should come from pressing a button on the AppBar
+    open(newVal) {
+      this.drawerOpen = newVal;
+    }
+  },
+  // Child -> Parent communication
+  methods: {
+    // Change 'isOpen' upon closing the navigation drawer
+    onInput(isOpen) {
+      this.$emit("drawer-change", isOpen);
+    }
+  }
+};
 </script>
